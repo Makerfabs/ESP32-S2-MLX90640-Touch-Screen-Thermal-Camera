e5357b0d4fbb5c293a161da090f0924e33088eb8
一九五 <2259761058@qq.com>
2021-12-22 09:08:35 +0800

    Add firmware V2

---
+++ firmware/Thermal_Camera_V2/Thermal_Camera_V2.ino
@@ -0,0 +1,527 @@
+#define LGFX_USE_V1
+
+#include <LovyanGFX.hpp>
+#include <Wire.h>
+#include <Adafruit_MLX90640.h>
+#include "color.h"
+#include "FT6236.h"
+#include "lcd_set.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+//Wifi Mode need use psram, and will very slow.Don't use.
+//#define WIFI_MODE
+
+#ifdef WIFI_MODE
+const char *ssid = "Makerfabs";
+const char *password = "20160704";
+
+WebServer server(80);
+#endif
+
+LGFX lcd;
+
+#define SPI_MOSI 2 //1
+#define SPI_MISO 41
+#define SPI_SCK 42
+#define SD_CS 1 //2
+
+#define I2C_SCL 39
+#define I2C_SDA 38
+
+#define MLX_I2C_ADDR 0x33
+Adafruit_MLX90640 mlx;
+
+const int i2c_touch_addr = TOUCH_I2C_ADD;
+#define get_pos ft6236_pos
+int touch_flag = 0;
+int pos[2] = {0, 0};
+
+#define MINTEMP 25
+#define MAXTEMP 37
+#define MLX_MIRROR 0 // Set 1 when the camera is facing the screen
+
+float frame[32 * 24]; // buffer for full frame of temperatures
+float *temp_frame = NULL;
+uint16_t *inter_p = NULL;
+
+void setup(void)
+{
+    pinMode(LCD_CS, OUTPUT);
+    pinMode(LCD_BLK, OUTPUT);
+
+    digitalWrite(LCD_CS, LOW);
+    digitalWrite(LCD_BLK, HIGH);
+
+    Serial.begin(115200);
+
+#ifdef WIFI_MODE
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    if (MDNS.begin("esp32"))
+    {
+        Serial.println("MDNS responder started");
+    }
+
+    server.on("/", handleRoot);
+    server.begin();
+    Serial.println("HTTP server started");
+
+#endif
+
+    Serial.println(ESP.getFreeHeap());
+
+    lcd.init();
+    lcd.setRotation(0);
+    display_ui();
+
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    if (SD_init())
+    {
+        Serial.println("SD init error");
+    }
+    appendFile(SD, "/temper.txt", "New Begin\n");
+
+    //I2C init
+    Wire.begin(I2C_SDA, I2C_SCL);
+    byte error, address;
+
+    Wire.beginTransmission(MLX_I2C_ADDR);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+        Serial.print("I2C device found at address 0x");
+        Serial.print(MLX_I2C_ADDR, HEX);
+        Serial.println("  !");
+    }
+    else if (error == 4)
+    {
+        Serial.print("Unknown error at address 0x");
+        Serial.println(MLX_I2C_ADDR, HEX);
+    }
+
+    Serial.println("Adafruit MLX90640 Simple Test");
+    if (!mlx.begin(MLX90640_I2CADDR_DEFAULT, &Wire))
+    {
+        Serial.println("MLX90640 not found!");
+        while (1)
+            delay(10);
+    }
+
+    //mlx.setMode(MLX90640_INTERLEAVED);
+    mlx.setMode(MLX90640_CHESS);
+    mlx.setResolution(MLX90640_ADC_18BIT);
+    mlx90640_resolution_t res = mlx.getResolution();
+    mlx.setRefreshRate(MLX90640_16_HZ);
+    mlx90640_refreshrate_t rate = mlx.getRefreshRate();
+    Wire.setClock(1000000); // max 1 MHz
+
+    Serial.println(ESP.getFreeHeap());
+    //image init
+    inter_p = (uint16_t *)malloc(320 * 240 * sizeof(uint16_t));
+    if (inter_p == NULL)
+    {
+        Serial.println("Malloc error");
+    }
+
+    for (int i = 0; i < 320 * 240; i++)
+    {
+        *(inter_p + i) = 0x480F;
+    }
+
+    Serial.println(ESP.getFreeHeap());
+    //frame init
+    temp_frame = (float *)malloc(32 * 24 * sizeof(float));
+    if (inter_p == NULL)
+    {
+        Serial.println("Malloc error");
+    }
+
+    for (int i = 0; i < 32 * 24; i++)
+    {
+        temp_frame[i] = MINTEMP;
+    }
+
+    Serial.println("All init over.");
+}
+
+uint32_t runtime = 0;
+int fps = 0;
+float max_temp = 0.0;
+int record_index = 0;
+
+void loop()
+{
+    //获取一帧
+    if (mlx.getFrame(frame) != 0)
+    {
+        Serial.println("Get frame failed");
+        return;
+    }
+
+    //和上次结果平均，滤波
+    filter_frame(frame, temp_frame);
+
+    //快排
+    qusort(frame, 0, 32 * 24 - 1);
+    max_temp += frame[767];
+
+    //温度矩阵转换图像矩阵，将32*24插值到320*240
+    interpolation(temp_frame, inter_p);
+    lcd.pushImage(0, 0, 320, 240, (lgfx::rgb565_t *)inter_p);
+
+    fps++;
+
+    if (get_pos(pos))
+    {
+        Serial.println((String) "x=" + pos[0] + ",y=" + pos[1]);
+        if (pos[0] > 210 && pos[1] > 400 && pos[0] < 320 && pos[1] < 480)
+        {
+            touch_flag = 1;
+        }
+    }
+
+    if ((millis() - runtime) > 2000)
+    {
+        lcd.fillRect(0, 280, 319, 79, TFT_BLACK);
+
+        lcd.setTextSize(4);
+        lcd.setCursor(0, 280);
+        lcd.setTextColor(camColors[map_f(max_temp / fps, MINTEMP, MAXTEMP)]);
+        lcd.println("  Max Temp:");
+        lcd.printf("  %6.1lf C", max_temp / fps);
+
+        lcd.setTextSize(1);
+        lcd.setTextColor(TFT_WHITE);
+        lcd.setCursor(0, 350);
+        lcd.printf("  FPS:%2.1lf", fps / 2.0);
+
+        if (touch_flag == 1)
+        {
+            touch_flag = 0;
+
+            lcd.fillRect(0, 380, 220, 80, TFT_BLACK);
+            lcd.setTextColor(TFT_WHITE);
+            lcd.setTextSize(1);
+            lcd.setCursor(10, 390);
+            lcd.printf("Record NO.%d to SD", record_index);
+
+            //Save Max temperture to sd card
+            char c[20];
+            sprintf(c, "[%d]\tT:%lf C\n", record_index++, max_temp / fps);
+            appendFile(SD, "/temper.txt", c);
+        }
+
+        runtime = millis();
+        max_temp = 0.0;
+        fps = 0;
+    }
+}
+
+//消抖并翻转
+//Filter temperature data and change camera direction
+void filter_frame(float *in, float *out)
+{
+    if (MLX_MIRROR == 1)
+    {
+        for (int i = 0; i < 32 * 24; i++)
+        {
+            out[i] = (out[i] + in[i]) / 2;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 24; i++)
+            for (int j = 0; j < 32; j++)
+            {
+                out[32 * i + 31 - j] = (out[32 * i + 31 - j] + in[32 * i + j]) / 2;
+            }
+    }
+}
+
+//32*24插值10倍到320*240
+//Transform 32*24 to 320 * 240 pixel
+void interpolation(float *data, uint16_t *out)
+{
+
+    for (uint8_t h = 0; h < 24; h++)
+    {
+        for (uint8_t w = 0; w < 32; w++)
+        {
+            out[h * 10 * 320 + w * 10] = map_f(data[h * 32 + w], MINTEMP, MAXTEMP);
+        }
+    }
+    for (int h = 0; h < 240; h += 10)
+    {
+        for (int w = 1; w < 310; w += 10)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                out[h * 320 + w + i] = (out[h * 320 + w - 1] * (9 - i) + out[h * 320 + w + 9] * (i + 1)) / 10;
+            }
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            out[h * 320 + 311 + i] = out[h * 320 + 310];
+        }
+    }
+    for (int w = 0; w < 320; w++)
+    {
+        for (int h = 1; h < 230; h += 10)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                out[(h + i) * 320 + w] = (out[(h - 1) * 320 + w] * (9 - i) + out[(h + 9) * 320 + w] * (i + 1)) / 10;
+            }
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            out[(231 + i) * 320 + w] = out[230 * 320 + w];
+        }
+    }
+    for (int h = 0; h < 240; h++)
+    {
+        for (int w = 0; w < 320; w++)
+        {
+            out[h * 320 + w] = camColors[out[h * 320 + w]];
+        }
+    }
+}
+
+//float to 0,255
+int map_f(float in, float a, float b)
+{
+    if (in < a)
+        return 0;
+
+    if (in > b)
+        return 255;
+
+    return (int)(in - a) * 255 / (b - a);
+}
+
+//Quick sort
+void qusort(float s[], int start, int end) //自定义函数 qusort()
+{
+    int i, j;        //定义变量为基本整型
+    i = start;       //将每组首个元素赋给i
+    j = end;         //将每组末尾元素赋给j
+    s[0] = s[start]; //设置基准值
+    while (i < j)
+    {
+        while (i < j && s[0] < s[j])
+            j--; //位置左移
+        if (i < j)
+        {
+            s[i] = s[j]; //将s[j]放到s[i]的位置上
+            i++;         //位置右移
+        }
+        while (i < j && s[i] <= s[0])
+            i++; //位置左移
+        if (i < j)
+        {
+            s[j] = s[i]; //将大于基准值的s[j]放到s[i]位置
+            j--;         //位置左移
+        }
+    }
+    s[i] = s[0]; //将基准值放入指定位置
+    if (start < i)
+        qusort(s, start, j - 1); //对分割出的部分递归调用qusort()函数
+    if (i < end)
+        qusort(s, j + 1, end);
+}
+
+void display_ui()
+{
+    for (int i = 0; i < 256; i++)
+        lcd.drawFastVLine(32 + i, 255, 20, camColors[i]);
+
+    lcd.setTextSize(2);
+    lcd.setCursor(5, 255);
+    lcd.println((String) "" + MINTEMP);
+
+    lcd.setCursor(290, 255);
+    lcd.println((String) "" + MAXTEMP);
+
+    lcd.fillRect(220, 380, 80, 80, TFT_GREEN);
+    lcd.setCursor(230, 390);
+    lcd.println("SAVE");
+
+#ifdef WIFI_MODE
+    lcd.setTextSize(1);
+    lcd.setCursor(10, 460);
+    lcd.println(WiFi.localIP());
+#endif
+}
+
+int SD_init()
+{
+
+    if (!SD.begin(SD_CS))
+    {
+        Serial.println("Card Mount Failed");
+        return 1;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if (cardType == CARD_NONE)
+    {
+        Serial.println("No SD card attached");
+        return 1;
+    }
+
+    Serial.print("SD Card Type: ");
+    if (cardType == CARD_MMC)
+    {
+        Serial.println("MMC");
+    }
+    else if (cardType == CARD_SD)
+    {
+        Serial.println("SDSC");
+    }
+    else if (cardType == CARD_SDHC)
+    {
+        Serial.println("SDHC");
+    }
+    else
+    {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+    listDir(SD, "/", 2);
+
+    Serial.println("SD init over.");
+    return 0;
+}
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
+{
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if (!root)
+    {
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if (!root.isDirectory())
+    {
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while (file)
+    {
+        if (file.isDirectory())
+        {
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if (levels)
+            {
+                listDir(fs, file.name(), levels - 1);
+            }
+        }
+        else
+        {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void readFile(fs::FS &fs, const char *path)
+{
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if (!file)
+    {
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while (file.available())
+    {
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if (!file)
+    {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if (file.print(message))
+    {
+        Serial.println("File written");
+    }
+    else
+    {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message)
+{
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if (!file)
+    {
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if (file.print(message))
+    {
+        Serial.println("Message appended");
+    }
+    else
+    {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+#ifdef WIFI_MODE
+
+void handleRoot()
+{
+    server.send(200, "text/plain", "hello from esp32!");
+}
+
+#endif
